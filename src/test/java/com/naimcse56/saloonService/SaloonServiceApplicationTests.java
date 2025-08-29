c5ad33e17ee80ba6e053c30883d495f7e1350761
unknown <naimcse56@gmail.com>
2025-08-29 21:19:48 +0600

    Saloon Service first intit

---
+++ src/test/java/com/naimcse56/saloonService/SaloonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naimcse56.saloonService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaloonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
